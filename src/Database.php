b6d78c1ae1586e68aa7fe56994ead165440e4f95
Alfonso Machado <alfonsomachado@spotahome.com>
2020-01-27 22:53:07 +0100

    example of remove else

---
+++ src/Database.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Example;
+
+class Database
+{
+    private $isUpdateReady;
+    private $isSyncCompleted;
+    private $isCacheEnabled;
+
+    public function __construct()
+    {
+        $this->isUpdateReady = false;
+        $this->isSyncCompleted = false;
+        $this->isCacheEnabled = false;
+    }
+
+    public function updateReady()
+    {
+        $this->isUpdateReady = true;
+    }
+
+    public function syncCompleted()
+    {
+        $this->isSyncCompleted = true;
+    }
+
+    public function cacheEnabled()
+    {
+        $this->isCacheEnabled = true;
+    }
+
+    /**
+     * A simple method handling the database operation related task
+     */
+    public function update(bool $isForceUpdate)
+    {
+        // isUpdateReady is class level variable
+        if ($this->isUpdateReady) {
+            // isForceUpdate is argument variable and based on this inner blocks is executed
+            if ($isForceUpdate) {
+                // isSyncCompleted is also class level variable, based on its true/false updateDbMain is called
+                // here updateBackupDb is called in both the cases
+                if ($this->isSyncCompleted) {
+                    $this->updateDbMain(true);
+                    $this->updateBackupDb(true);
+                } else {
+                    $this->updateDbMain(false);
+                    $this->updateBackupDb(true);
+                }
+            } else {
+                // execute this if isUpdateReady is false i. e., this is dependent on if condition
+                $this->updateCache(!$this->isCacheEnabled);
+
+                // end of second isForceUpdate block
+            }
+            // end of first if block
+        }
+        // end of method
+    }
+
+    private function updateDbMain(bool $update)
+    {
+    }
+
+    private function updateBackupDb(bool $update)
+    {
+    }
+
+    private function updateCache(bool $update)
+    {
+    }
+}
