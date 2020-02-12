1c0511561febad3ca3408ec26ede05cf0cf8a592
Alfonso Machado <alfonsomachado@spotahome.com>
2020-02-12 08:36:50 +0100

    liskov principle

---
+++ bin/birds.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+
+require_once(__DIR__.'/../vendor/autoload.php');
+
+// 1. Dibuja una avestruz
+
+// 2. Dibuja una gallina.
+
+// 3. Dibuja un pajaro.
+
+$birds = []; // list of birds;
+foreach ($birds as $bird) {
+    // todo
+    // Muestra por patanlla nombre del pajaro y posición.
+}
---
+++ src/Bird/Chicken.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Bird;
+
+class Chicken
+{
+}
